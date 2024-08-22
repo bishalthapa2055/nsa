81589cf5432f702bb2cbe74018d076ce9ec46617
bishalthapa2055 <bishalthapa2055@gmail.com>
2024-08-22 11:38:45 +0545

    added verify otp handler

--- src/routes/v1/auth/arena/index.routes.ts
+++ src/routes/v1/auth/arena/index.routes.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import { sendOtpArenaRouter } from "./sendOtp.routes";
+import { verifyOtpArenaRouter } from "./verifyOtp.routes";
 
 const router = express.Router();
 
 router.use(sendOtpArenaRouter); // send otp routes
+router.use(verifyOtpArenaRouter); //verify otp routes
 
 
 export { router as indexAuthArenaRouter }
---
+++ src/routes/v1/auth/arena/verifyOtp.routes.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import { verifyOtpArenaControllerHandler } from "../../../../controllers/auth/arena/verifyOtp.controller";
+import { validateRequest } from "../../../../common/middlewares/validate-request";
+import { body } from "express-validator";
+
+const router = express.Router();
+
+router.post(
+    '/verify-otp',
+    [
+        body("phone_number").not().isEmpty().withMessage("Phone Number field is required !!!")
+        .custom((value) => {
+            // Validate the phone number starts with +977
+            if (!value.startsWith("+977")) {
+              throw new Error("Phone number must start with +977.");
+            }
+            
+            // Validate the remaining 10 digits
+            const remainingPart = value.slice(4); // Extract part after +977
+            const remainingRegex = /^9\d{9}$/;
+            
+            if (!remainingRegex.test(remainingPart)) {
+              throw new Error("After +977, the phone number must start with 9 and be followed by 9 digits.");
+            }
+            
+            return true;
+        }),
+        body("otp_code")
+        .not().isEmpty().withMessage("OTP code is required.")
+        .isNumeric().withMessage("OTP code must be numeric.")
+        .isLength({ min: 6, max: 6 }).withMessage("OTP code must be exactly 6 digits.")
+    ],
+    validateRequest,
+    verifyOtpArenaControllerHandler
+)
+
+export { router as verifyOtpArenaRouter}
