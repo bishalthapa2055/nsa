7f2e952181503227bb99b59ea94c76a322c104d2
bishalthapa2055 <bishalthapa2055@gmail.com>
2024-08-28 17:59:17 +0545

    added weekly available scheudle model

---
+++ src/model/open_end_time.ts
@@ -0,0 +1,36 @@
+// timeSchedule.model.ts
+import mongoose, { Schema, Document } from 'mongoose';
+
+
+export interface TimeSchedule {
+    day: string;         // Day of the week
+    startTime: string;   // Start time in HH:mm  24 Hr format
+    endTime: string;     // End time in HH:mm  24 Hr format
+}
+
+
+// Define a TypeScript interface for the document
+interface IWeeklySchedule extends Document {
+    schedules: TimeSchedule[];
+}
+
+// Create a schema definition
+const TimeScheduleSchema: Schema = new Schema({
+    day: { type: String, required: true, enum: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'] },
+    startTime: { type: String, required: true },
+    endTime: { type: String, required: true },
+});
+
+// Create a schema for the weekly schedule
+const WeeklyScheduleSchema: Schema = new Schema({
+    schedules: {
+        type: [TimeScheduleSchema],
+        required: true,
+        validate: [(val: TimeSchedule[]) => val.length === 7, 'Schedules must have 7 days.']
+    },
+});
+
+// Create a Mongoose model
+const WeeklySchedule = mongoose.model<IWeeklySchedule>('WeeklySchedule', WeeklyScheduleSchema);
+
+export default WeeklySchedule;
